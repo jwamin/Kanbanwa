eab4753c6503918ab564fe1ea289c7423bd42423
Joss Manger <jossferatu@gmail.com>
2020-08-03 17:59:46 -0400

    Initial Commit

---
+++ Kanbanwa/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Kanbanwa
+//
+//  Created by Joss Manger on 8/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kanbanwa/KanbanwaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanbanwaApp.swift
+//  Kanbanwa
+//
+//  Created by Joss Manger on 8/3/20.
+//
+
+import SwiftUI
+
+@main
+struct KanbanwaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
